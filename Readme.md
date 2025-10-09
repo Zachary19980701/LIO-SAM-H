92f34e213f6c067c12da20923bc4252d56a439cf
Zachary19980701 <15536896309@163.com>
2025-10-09 03:05:48 +0000

    初始化仓库

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# LIO-SAM-H
+这是hzy的LIO-SAM的代码复现版本
---
+++ include/utility.h
@@ -0,0 +1,352 @@
+// 传感器的参数管理、数据格式转换和部分的数据处理的过程
+#pragma once // 防止头文件被重复包含
+#ifndef _UTILITY_LIDAR_ODOMETRY_H_
+#define _UTILITY_LIDAR_ODOMETRY_H_
+#define PCL_NO_PRECOMPILE 
+
+
+#include <ros/ros.h>  // ros
+
+// ros message types
+#include <std_msgs/Header.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+// opencv 
+#include <opencv2/opencv.hpp>
+
+// pcl
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/search/impl/search.hpp>
+#include <pcl/range_image/range_image.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/registration/icp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/crop_box.h> 
+#include <pcl_conversions/pcl_conversions.h>
+
+// tf 
+#include <tf/linearMath/Quaternion.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+// standard cpp lib
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cfloat>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <limits>
+#include <iomanip>
+#include <array>
+#include <thread>
+#include <mutex>
+
+typedef pcl::PointXYZI PointType; // rename pcl::PointXYZI to PointType
+
+enum class SensorType {VELODYNE, OUSTER, LIVOX};
+
+// 参数读取
+class ParamServer{
+  public:
+    ros::NodeHandle nh; // ros node handle
+
+    std::string robot_id;
+
+    // Topics
+    std::string pointCloudTopic;
+    std::string imuTopic;
+    std::string odomTopic;
+    std::string gpsTopic;
+
+    // Frames
+    std::string lidarFrame;
+    std::string baselinkFrame;
+    std::string odometryFrame;
+    std::string mapFrame;
+
+    //gps settings
+    bool uesImuHeadingInitiallization;
+    bool useGpsElevation;
+    float gpsCovThreshold;
+    float poseCovThreshold;
+
+    // save pcd
+    bool savePCD;
+    std::string savePCDDirectory;
+
+    // Lidar configuration
+    SensorType sensor;
+    int N_SCAN; // 激光雷达的线数
+    int Horizon_SCAN; // 每个扫描线上的点数
+    int downsampleRate; // 点云降采样率
+    float lidarMinRange; // 点云的最小距离
+    float lidarMaxRange; // 点云的最大距离
+
+    // IMU configuration
+    float imuAccNoise; // 加速度噪声
+    float imuGyrNoise; // 陀螺仪噪声
+    float imuAccBiasN; // 加速度偏置
+    float imuGyrBiasN; // 陀螺仪偏置
+    float imuGravity; // 重力加速度
+    float imuRPYWeight; // 加速度和陀螺仪的权重
+    std::vector<double> extRotV; // 外部旋转
+    std::vector<double> extRPYV; // 外部旋转
+    std::vector<double> extTransV; // 外部平移
+    Eigen::Matrix3d extRot; // 外部旋转
+    Eigen::Matrix3d extRPY; // 外部旋转
+    Eigen::Vector3d extTrans; //外部平移
+    Eigen::Quaterniond extQRPY; // 外部旋转的四元数
+
+    // LOAM
+    float edgeThreshold;
+    float surfThreshold;
+    int edgeFeatureMinValidNum;
+    int surfFeatureMinValidNum;
+
+    // voxel filter paprams
+    float odometrySurfLeafSize;
+    float mappingCornerLeafSize;
+    float mappingSurfLeafSize ;
+
+    float z_tollerance; 
+    float rotation_tollerance;
+
+    // CPU params
+    int numberOfCores; // 核心数
+    double mappingProcessInterval; // 映射过程的时间间隔
+
+    // Surrounding map
+    float surroundingkeyframeAddingDistThreshold; 
+    float surroundingkeyframeAddingAngleThreshold; 
+    float surroundingKeyframeDensity;
+    float surroundingKeyframeSearchRadius;
+
+    // Loop closure
+    bool  loopClosureEnableFlag; // 是否开启闭环
+    float loopClosureFrequency; // 闭环频率
+    int   surroundingKeyframeSize; // 闭环关键帧的大小
+    float historyKeyframeSearchRadius; // 闭环关键帧的搜索半径
+    float historyKeyframeSearchTimeDiff; // 闭环关键帧的时间差
+    int   historyKeyframeSearchNum; // 闭环关键帧的搜索数量
+    float historyKeyframeFitnessScore; // 闭环关键帧的适应度分数
+
+    // global map visualization radius 全局地图的可视半径
+    float globalMapVisualizationSearchRadius;
+    float globalMapVisualizationPoseDensity;
+    float globalMapVisualizationLeafSize;
+
+    // param server
+    ParamServer()
+    {
+        nh.param<std::string>("/robot_id", robot_id, "roboat");
+
+        nh.param<std::string>("lio_sam/pointCloudTopic", pointCloudTopic, "points_raw");
+        nh.param<std::string>("lio_sam/imuTopic", imuTopic, "imu_correct");
+        nh.param<std::string>("lio_sam/odomTopic", odomTopic, "odometry/imu");
+        nh.param<std::string>("lio_sam/gpsTopic", gpsTopic, "odometry/gps");
+
+        nh.param<std::string>("lio_sam/lidarFrame", lidarFrame, "base_link");
+        nh.param<std::string>("lio_sam/baselinkFrame", baselinkFrame, "base_link");
+        nh.param<std::string>("lio_sam/odometryFrame", odometryFrame, "odom");
+        nh.param<std::string>("lio_sam/mapFrame", mapFrame, "map");
+
+        nh.param<bool>("lio_sam/useImuHeadingInitialization", useImuHeadingInitialization, false);
+        nh.param<bool>("lio_sam/useGpsElevation", useGpsElevation, false);
+        nh.param<float>("lio_sam/gpsCovThreshold", gpsCovThreshold, 2.0);
+        nh.param<float>("lio_sam/poseCovThreshold", poseCovThreshold, 25.0);
+
+        nh.param<bool>("lio_sam/savePCD", savePCD, false);
+        nh.param<std::string>("lio_sam/savePCDDirectory", savePCDDirectory, "/Downloads/LOAM/");
+
+        std::string sensorStr;
+        nh.param<std::string>("lio_sam/sensor", sensorStr, "");
+        if (sensorStr == "velodyne")
+        {
+            sensor = SensorType::VELODYNE;
+        }
+        else if (sensorStr == "ouster")
+        {
+            sensor = SensorType::OUSTER;
+        }
+        else if (sensorStr == "livox")
+        {
+            sensor = SensorType::LIVOX;
+        }
+        else
+        {
+            ROS_ERROR_STREAM(
+                "Invalid sensor type (must be either 'velodyne' or 'ouster' or 'livox'): " << sensorStr);
+            ros::shutdown();
+        }
+
+        nh.param<int>("lio_sam/N_SCAN", N_SCAN, 16);
+        nh.param<int>("lio_sam/Horizon_SCAN", Horizon_SCAN, 1800);
+        nh.param<int>("lio_sam/downsampleRate", downsampleRate, 1);
+        nh.param<float>("lio_sam/lidarMinRange", lidarMinRange, 1.0);
+        nh.param<float>("lio_sam/lidarMaxRange", lidarMaxRange, 1000.0);
+
+        nh.param<float>("lio_sam/imuAccNoise", imuAccNoise, 0.01);
+        nh.param<float>("lio_sam/imuGyrNoise", imuGyrNoise, 0.001);
+        nh.param<float>("lio_sam/imuAccBiasN", imuAccBiasN, 0.0002);
+        nh.param<float>("lio_sam/imuGyrBiasN", imuGyrBiasN, 0.00003);
+        nh.param<float>("lio_sam/imuGravity", imuGravity, 9.80511);
+        nh.param<float>("lio_sam/imuRPYWeight", imuRPYWeight, 0.01);
+        nh.param<vector<double>>("lio_sam/extrinsicRot", extRotV, vector<double>());
+        nh.param<vector<double>>("lio_sam/extrinsicRPY", extRPYV, vector<double>());
+        nh.param<vector<double>>("lio_sam/extrinsicTrans", extTransV, vector<double>());
+
+        // 外参转换参数
+        extRot = Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(extRotV.data(), 3, 3);
+        extRPY = Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(extRPYV.data(), 3, 3);
+        extTrans = Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(extTransV.data(), 3, 1);
+        extQRPY = Eigen::Quaterniond(extRPY).inverse();
+
+        nh.param<float>("lio_sam/edgeThreshold", edgeThreshold, 0.1);
+        nh.param<float>("lio_sam/surfThreshold", surfThreshold, 0.1);
+        nh.param<int>("lio_sam/edgeFeatureMinValidNum", edgeFeatureMinValidNum, 10);
+        nh.param<int>("lio_sam/surfFeatureMinValidNum", surfFeatureMinValidNum, 100);
+
+        nh.param<float>("lio_sam/odometrySurfLeafSize", odometrySurfLeafSize, 0.2);
+        nh.param<float>("lio_sam/mappingCornerLeafSize", mappingCornerLeafSize, 0.2);
+        nh.param<float>("lio_sam/mappingSurfLeafSize", mappingSurfLeafSize, 0.2);
+
+        nh.param<float>("lio_sam/z_tollerance", z_tollerance, FLT_MAX);
+        nh.param<float>("lio_sam/rotation_tollerance", rotation_tollerance, FLT_MAX);
+
+        nh.param<int>("lio_sam/numberOfCores", numberOfCores, 2);
+        nh.param<double>("lio_sam/mappingProcessInterval", mappingProcessInterval, 0.15);
+
+        nh.param<float>("lio_sam/surroundingkeyframeAddingDistThreshold", surroundingkeyframeAddingDistThreshold, 1.0);
+        nh.param<float>("lio_sam/surroundingkeyframeAddingAngleThreshold", surroundingkeyframeAddingAngleThreshold, 0.2);
+        nh.param<float>("lio_sam/surroundingKeyframeDensity", surroundingKeyframeDensity, 1.0);
+        nh.param<float>("lio_sam/surroundingKeyframeSearchRadius", surroundingKeyframeSearchRadius, 50.0);
+
+        nh.param<bool>("lio_sam/loopClosureEnableFlag", loopClosureEnableFlag, false);
+        nh.param<float>("lio_sam/loopClosureFrequency", loopClosureFrequency, 1.0);
+        nh.param<int>("lio_sam/surroundingKeyframeSize", surroundingKeyframeSize, 50);
+        nh.param<float>("lio_sam/historyKeyframeSearchRadius", historyKeyframeSearchRadius, 10.0);
+        nh.param<float>("lio_sam/historyKeyframeSearchTimeDiff", historyKeyframeSearchTimeDiff, 30.0);
+        nh.param<int>("lio_sam/historyKeyframeSearchNum", historyKeyframeSearchNum, 25);
+        nh.param<float>("lio_sam/historyKeyframeFitnessScore", historyKeyframeFitnessScore, 0.3);
+
+        nh.param<float>("lio_sam/globalMapVisualizationSearchRadius", globalMapVisualizationSearchRadius, 1e3);
+        nh.param<float>("lio_sam/globalMapVisualizationPoseDensity", globalMapVisualizationPoseDensity, 10.0);
+        nh.param<float>("lio_sam/globalMapVisualizationLeafSize", globalMapVisualizationLeafSize, 1.0);
+
+        usleep(100);
+    }
+
+    sensor_msgs::Imu imuConverter(const sensor_msgs::Imu& imu_in){
+      sensor_msgs::Imu imu_out = = imu_in;
+      // 旋转加速度
+      Eigen::Vector3d acc(imu_in.linear_acceleration.x, imu_in.linear_acceleration.y, imu_in.linear_acceleration.z); // 三轴角加速度
+      acc = extRot * acc; // 旋转加速度
+      imu_out.linear_acceleration.x = acc.x();
+      imu_out.linear_acceleration.y = acc.y();
+      imu_out.linear_acceleration.z = acc.z();
+
+      // 旋转陀螺仪
+      Eigen::Vector3d gyr(imu_in.angular_velocity.x, imu_in.angular_velocity.y, imu_in.angular_velocity.z); // 三轴角速度
+      gyr = extRot * gyr; // 旋转陀螺仪
+      imu_out.angular_velocity.x = gyr.x();
+      imu_out.angular_velocity.y = gyr.y();
+      imu_out.angular_velocity.z = gyr.z();
+
+      // 姿态转换
+      Eigen::Quaterniond q_from(imu_in.orientation.w, imu_in.orientation.x, imu_in.orientation.y, imu_in.orientation.z);
+      Eigen::Quaterniond q_final = q_from * extQRPY;
+      imu_out.orientation.x = q_final.x();
+      imu_out.orientation.y = q_final.y();
+      imu_out.orientation.z = q_final.z();
+      imu_out.orientation.w = q_final.w();
+
+      if(sqrt(q_final.x()*q_final.x() + q_final.y()*q_final.y() + q_final.z()*q_final.z() + q_final.w()*q_final.w()) < 0.1)
+      {
+          ROS_ERROR("Invalid quaternion, please use a 9-axis IMU!");
+          ros::shutdown();
+      }
+
+      return imu_out;
+
+    }
+};
+
+template<typename T>
+sensor_msgs::PointCloud2 publishCloud(const ros::Publisher& thisPub, const T& thisCloud, ros::Time thisStamp, std::string thisFrame){
+  sensor_msgs::PointCloud2 tempCloud;
+  pcl::toROSMsg(*thisCloud, tempCloud);
+  tempCloud.header.stamp = thisStamp;
+  tempCloud.header.frame_id = thisFrame;
+  if(thisPub.getNumSubscribers() != 0)
+    thisPub.publish(tempCloud);
+  return tempCloud;
+}
+
+template<typename T>
+double ROS_TIME(T msg){
+  return msg->header.stamp.toSec();
+}
+
+
+template<typename T>
+void imuAcce2rosAccel(sensor_msgs::Imu* thisImuMsg, T* angular_x, T* angular_y, T* angular_z){
+  *angular_x = thisImuMsg->angular_velocity.x;
+  *angular_y = thisImuMsg->angular_velocity.y;
+  *angular_z = thisImuMsg->angular_velocity.z;
+}
+
+template<typename T>
+void imuAccel2rosAccel(sensor_msgs::Imu *thisImuMsg, T *acc_x, T *acc_y, T *acc_z)
+{
+    *acc_x = thisImuMsg->linear_acceleration.x;
+    *acc_y = thisImuMsg->linear_acceleration.y;
+    *acc_z = thisImuMsg->linear_acceleration.z;
+}
+
+template<typename T>
+void imuRPY2rosRPY(sensor_msgs::Imu *thisImuMsg, T *rosRoll, T *rosPitch, T *rosYaw)
+{
+    double imuRoll, imuPitch, imuYaw;
+    tf::Quaternion orientation;
+    tf::quaternionMsgToTF(thisImuMsg->orientation, orientation);
+    tf::Matrix3x3(orientation).getRPY(imuRoll, imuPitch, imuYaw);
+
+    *rosRoll = imuRoll;
+    *rosPitch = imuPitch;
+    *rosYaw = imuYaw;
+}
+
+float pointDistance(PointType p)
+{
+    return sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
+}
+
+
+float pointDistance(PointType p1, PointType p2)
+{
+    return sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) + (p1.z-p2.z)*(p1.z-p2.z));
+}
+
+
+
+#endif
